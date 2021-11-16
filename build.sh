f040dca72cd76422b527125807ce8f5d4d56b375
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-11-17 01:01:45 +0530

    build.sh: Add standalone kernel build script

---
+++ build.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#
+# Compile script for QuicksilveR kernel
+# Copyright (C) 2020-2021 Adithya R.
+
+SECONDS=0 # builtin bash timer
+ZIPNAME="QuicksilveR-X3-v2-$(date '+%Y%m%d-%H%M').zip"
+TC_DIR="$HOME/aosp/prebuilts/clang/host/linux-x86/clang-r383902"
+GCC_64_DIR="$HOME/tc/aarch64-linux-android-4.9"
+GCC_32_DIR="$HOME/tc/arm-linux-androideabi-4.9"
+AK3_DIR="$HOME/android/AnyKernel3"
+DEFCONFIG="vendor/x3-perf_defconfig"
+
+export PATH="$TC_DIR/bin:$PATH"
+
+if [[ $1 = "-r" || $1 = "--regen" ]]; then
+	make O=out ARCH=arm64 $DEFCONFIG savedefconfig
+	cp out/defconfig arch/arm64/configs/$DEFCONFIG
+	echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
+	exit
+fi
+
+if [[ $1 = "-c" || $1 = "--clean" ]]; then
+	rm -rf out
+fi
+
+mkdir -p out
+make O=out ARCH=arm64 $DEFCONFIG
+
+echo -e "\nStarting compilation...\n"
+make -j$(nproc --all) O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=$GCC_64_DIR/bin/aarch64-linux-android- CROSS_COMPILE_ARM32=$GCC_32_DIR/bin/arm-linux-androideabi- Image.gz dtbo.img
+
+kernel="out/arch/arm64/boot/Image.gz"
+dtb="out/arch/arm64/boot/dts/qcom/sm8150-v2.dtb"
+dtbo="out/arch/arm64/boot/dtbo.img"
+
+if [ -f "$kernel" ] && [ -f "$dtb" ] && [ -f "$dtbo" ]; then
+	echo -e "\nKernel compiled succesfully! Zipping up...\n"
+	if [ -d "$AK3_DIR" ]; then
+		cp -r $AK3_DIR AnyKernel3
+	elif ! git clone -q https://github.com/ghostrider-reborn/AnyKernel3 -b x3; then
+		echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
+		exit 1
+	fi
+	cp $kernel $dtbo AnyKernel3
+	cp $dtb AnyKernel3/dtb
+	rm -rf out/arch/arm64/boot
+	cd AnyKernel3
+	git checkout x3 &> /dev/null
+	zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
+	cd ..
+	rm -rf AnyKernel3
+	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+	echo "Zip: $ZIPNAME"
+	if ! [[ $HOSTNAME = "RyzenBeast" && $USER = "adithya" ]]; then
+		if [ -x "$(command -v gdrive)" ]; then
+			gdrive upload --share "$ZIPNAME"
+		else
+			curl --upload-file "$ZIPNAME" http://transfer.sh/"$ZIPNAME"
+			echo
+		fi
+	fi
+else
+	echo -e "\nCompilation failed!"
+	exit 1
+fi
